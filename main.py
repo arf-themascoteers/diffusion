ae1fa067ff1cad99b144bdb5928eae5cbc0b5447
arif <arif@themascoteers.com>
2023-12-02 18:12:43 +1100

    done

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
+import torch
+from denoising_diffusion_pytorch import Unet, GaussianDiffusion
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+model = Unet(
+    dim = 64,
+    dim_mults = (1, 2, 4, 8),
+    flash_attn = True
+)
 
+diffusion = GaussianDiffusion(
+    model,
+    image_size = 128,
+    timesteps = 1000    # number of steps
+)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+training_images = torch.rand(8, 3, 128, 128) # images are normalized from 0 to 1
+loss = diffusion(training_images)
+loss.backward()
 
+# after a lot of training
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+sampled_images = diffusion.sample(batch_size = 4)
+print(sampled_images.shape) # (4, 3, 128, 128)
+sampled_images = sampled_images.cpu().detach()
+# Save the tensor to a file using torch.save
+torch.save(sampled_images, 'saved_tensor.pt')
